970b87a87c4b02aab46e68e382cae86ef1a2f0fc
cut-the-freakin-steak <calvaradolopez@proton.me>
2025-12-10 09:44:22 -0700

    added + completed high score

--- rust/src/global_settings.rs
+++ rust/src/global_settings.rs
@@ -1,9 +1,9 @@
 // NOTE: done with this file
 
 use godot::classes::{
-    DisplayServer, Engine, FileAccess, INode, Json, Node, RefCounted, display_server, file_access,
+    ConfigFile, DisplayServer, Engine, FileAccess, INode, Node, RefCounted, display_server,
 };
-use godot::global::{Error, printerr};
+use godot::global::Error;
 use godot::prelude::*;
 
 #[derive(GodotClass)]
@@ -20,7 +20,7 @@ pub struct SettingsClass {
     #[init(val = OnReady::manual())]
     pub sfx_bus: OnReady<Gd<RefCounted>>,
 
-    #[init(val = "user://settings.json".to_godot())]
+    #[init(val = "user://settings.cfg".to_godot())]
     pub settings_save_path: GString,
 
     #[init(val = true)]
@@ -80,33 +80,39 @@ impl INode for SettingsClass {
 }
 
 impl SettingsClass {
-    pub fn save_settings(&mut self) {
+    pub fn save_settings(&self) {
         println!("tit"); // idk
 
-        let settings_dict = vdict! {
-            "vsync_on": self.vsync_on,
-            "screen_shake_on": self.screen_shake_on,
-            "hurricane_mode": self.hurricane_mode,
-            "master_volume": self.master_volume,
-            "music_volume": self.music_volume,
-            "sfx_volume": self.sfx_volume,
-            "high_score": self.high_score,
-        };
+        let mut config_file = ConfigFile::new_gd();
 
-        let json_string = Json::stringify(&settings_dict.to_variant());
-        let file_opt = FileAccess::open(&self.settings_save_path, file_access::ModeFlags::WRITE);
-        let mut file;
+        // settings section
+        config_file.set_value("settings", "vsync_on", &self.vsync_on.to_variant());
+        config_file.set_value(
+            "settings",
+            "screen_shake_on",
+            &self.screen_shake_on.to_variant(),
+        );
+        config_file.set_value(
+            "settings",
+            "hurricane_mode",
+            &self.hurricane_mode.to_variant(),
+        );
+        config_file.set_value(
+            "settings",
+            "master_volume",
+            &self.master_volume.to_variant(),
+        );
+        config_file.set_value("settings", "music_volume", &self.music_volume.to_variant());
+        config_file.set_value("settings", "sfx_volume", &self.sfx_volume.to_variant());
 
-        if let Some(returned_file) = file_opt {
-            file = returned_file
+        // game section
+        config_file.set_value("game", "high_score", &self.high_score.to_variant());
+
+        let err = config_file.save(&self.settings_save_path);
+
+        if err != Error::OK {
+            godot_error!("failed to save settings");
         }
-        else {
-            printerr(&["file failed to load lmao".to_variant()]);
-            return;
-        }
-
-        file.store_string(&json_string);
-        file.close();
     }
 
     pub fn load_settings(&mut self) {
@@ -115,76 +121,51 @@ impl SettingsClass {
             return;
         }
 
-        let file_opt = FileAccess::open(&self.settings_save_path, file_access::ModeFlags::READ);
-        let mut file;
+        let mut config_file = ConfigFile::new_gd();
 
-        if let Some(returned_file) = file_opt {
-            file = returned_file
+        let err = config_file.load(&self.settings_save_path);
+
+        if err != Error::OK {
+            godot_error!("failed to load settings");
         }
-        else {
-            printerr(&["file failed to load lmao".to_variant()]);
-            return;
-        }
-
-        let json_string = file.get_as_text();
-        file.close();
-
-        let mut json = Json::new_gd();
-
-        // parse json and check result to make sure settings aren't corrupt
-        let parsed_results = json.parse(&json_string);
-
-        if parsed_results != Error::OK {
-            printerr(&["failed to parse json".to_variant()]);
-            return;
-        }
-
-        let settings_dict = json.get_data().try_to::<VarDictionary>().unwrap();
 
         // load each setting and update variable values with a fallback value just in case
-        self.vsync_on = settings_dict
-            .get("vsync_on")
-            .unwrap()
+        self.vsync_on = config_file
+            .get_value("settings", "vsync_on")
             .try_to::<bool>()
-            .unwrap();
+            .unwrap_or(true);
 
-        self.screen_shake_on = settings_dict
-            .get("screen_shake_on")
-            .unwrap()
+        self.screen_shake_on = config_file
+            .get_value("settings", "screen_shake_on")
             .try_to::<bool>()
-            .unwrap();
+            .unwrap_or(true);
 
-        self.hurricane_mode = settings_dict
-            .get("hurricane_mode")
-            .unwrap()
+        self.hurricane_mode = config_file
+            .get_value("settings", "hurricane_mode")
             .try_to::<bool>()
-            .unwrap();
+            .unwrap_or(false);
 
-        self.master_volume = settings_dict
-            .get("master_volume")
-            .unwrap()
+        self.master_volume = config_file
+            .get_value("settings", "master_volume")
             .try_to::<f64>()
-            .unwrap();
+            .unwrap_or(1.0);
 
-        self.music_volume = settings_dict
-            .get("music_volume")
-            .unwrap()
+        self.music_volume = config_file
+            .get_value("settings", "music_volume")
             .try_to::<f64>()
-            .unwrap();
+            .unwrap_or(1.0);
 
-        self.sfx_volume = settings_dict
-            .get("sfx_volume")
-            .unwrap()
+        self.sfx_volume = config_file
+            .get_value("settings", "sfx_volume")
             .try_to::<f64>()
-            .unwrap();
+            .unwrap_or(1.0);
 
         // NOTE: this is bullshit, you have to always take it out as a float and then cast it to an
         // int
-        self.high_score = settings_dict
-            .get("high_score")
-            .unwrap()
-            .try_to::<f64>()
-            .unwrap() as i64;
+        self.high_score = config_file
+            .get_value("game", "high_score")
+            .try_to::<i64>()
+            .unwrap_or(0);
 
         // any settings not seen here handle their behaviour based on a variable or function that isn't the direct variable that we set above
         match self.vsync_on {
